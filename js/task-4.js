267e4bcbfabf1044fe942acee1637544aa53ca9c
vivivihadi <oliynykvika07@gmail.com>
2025-08-16 13:37:19 +0300

    js

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+function getShippingCost(country) {
+  switch (country) {
+    case "China":
+      return "Shipping to China will cost 100 credits";
+
+    case "Chile":
+      return "Shipping to Chile will cost 250 credits";
+
+    case "Australia":
+      return "Shipping to Australia will cost 170 credits";
+
+    case "Jamaica":
+      return "Shipping to Jamaica will cost 120 credits";
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+}
+
+console.log(getShippingCost("Australia")); 
+console.log(getShippingCost("Germany")); 
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica")); 
+console.log(getShippingCost("Sweden")); 
